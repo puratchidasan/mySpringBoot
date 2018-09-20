2b33cf3e80c78eeb98406a78c6627acfbdc8ab73
Puratchidasan Munusami <puratchidasan@gmail.com>
2018-09-20 22:27:05 +0200

    Update ElkExampleSpringBootApplication.java

--- tavant-spring-boot/src/main/java/com/tavant/spring/boot/ElkExampleSpringBootApplication.java
+++ tavant-spring-boot/src/main/java/com/tavant/spring/boot/ElkExampleSpringBootApplication.java
@@ -15,7 +15,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-@SpringBootApplication
+//@SpringBootApplication
 public class ElkExampleSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(ElkExampleSpringBootApplication.class, args);
